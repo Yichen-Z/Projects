56df91ba49548c98afa5c45bbae9658d40a8c73e
Yichen Zhang <zhangyichen9@gmail.com>
2020-07-22 01:46:17 -0400

    Created navigation bar. Starting to experiment with connection to Airtable

--- airtable-project/README.md
+++ airtable-project/README.md
@@ -1,11 +1,13 @@
 # airtable-project
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
---
+++ airtable-project/src/components/AirtableTalker.vue
@@ -0,0 +1,21 @@
+<template>
+<axios></axios>
+</template>
+
+<script>
+import axios from 'axios';
+
+axios({
+    url: 'https://airtable.com/shrKWXmiLy6YZnZxx'
+}).then((res) => {
+    res.data.records
+})
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
