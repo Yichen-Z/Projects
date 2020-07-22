04fca2fc4451a929af83c7b9e43ab3b6bc805de9
Yichen Zhang <zhangyichen9@gmail.com>
2020-07-22 00:13:14 -0400

    Begin experiment to use Vue to incorporate Airtable data to a webpage. Incorporated Bootstrap menu bar.

---
+++ airtable-project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <nav-bar></nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+
+export default {
+  name: 'navbar',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#router-view {
+  padding: 20px 0px;
+  margin: 0 auto;
+}
+</style>
